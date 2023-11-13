a5cfceb5454c75e4c260a4cbdd5cec90fa0fea83
Missing-Tech <josephgrabski31@gmail.com>
2023-11-13 17:46:20 +0000

    Download grib data

---
+++ cds.py
@@ -0,0 +1,16 @@
+import cdsapi
+
+c = cdsapi.Client()
+
+c.retrieve("reanalysis-era5-complete", {
+    "class": "ea",
+    "date": "2023-03-31",
+    "expver": "1",
+    "levelist": "200/225/250/300",
+    "levtype": "pl",
+    "param": "130.128/131/132/157.128",
+    "step": "0",
+    "stream": "oper",
+    "time": "09:00:00",
+    "type": "4v"
+}, "output")
