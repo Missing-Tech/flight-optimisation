30e856b0d2e168edb8aa06d6a06322aae8e4c603
Missing-Tech <josephgrabski31@gmail.com>
2023-11-19 13:58:21 +0000

    Format with Ruff

--- parse_grib.py
+++ parse_grib.py
@@ -1,28 +1,28 @@
-import pandas
 import xarray as xr
 
-ds = xr.open_dataset('output.grib', engine='cfgrib')
+ds = xr.open_dataset("output.grib", engine="cfgrib")
 
 # Using https://spire.com/tutorial/spire-weather-tutorial-intro-to-processing-grib2-data-with-python/
 
 # Access wind vector data
-u_component = ds['u']
-v_component = ds['v']
+u_component = ds["u"]
+v_component = ds["v"]
 
-ds = ds.get(['u', 'v', 'latitude', 'longitude'])
+ds = ds.get(["u", "v", "latitude", "longitude"])
 
-df =  ds.to_dataframe()
+df = ds.to_dataframe()
 
 latitudes = df.index.get_level_values("latitude")
 longitudes = df.index.get_level_values("longitude")
 
-map_function = lambda lon: (lon - 360) if (lon > 180) else lon
-remapped_longitudes = longitudes.map(map_function)
+
+def center_longitudes(lon):
+    return lon - 360 if lon > 180 else lon
+
+
+remapped_longitudes = longitudes.map(center_longitudes)
 
 df["longitude"] = remapped_longitudes
-df["latitude"] = latitudes  
+df["latitude"] = latitudes
 
 df.to_csv("output_data.csv", index=False)
-
-# def get_wind_data():
-#     return data
