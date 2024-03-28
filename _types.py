a5e475a4a7dace6f2e2270f9b7bcd2c5910ed2a9
Missing-Tech <josephgrabski31@gmail.com>
2024-03-28 10:46:06 +0000

    Add types for flight.py

--- _types.py
+++ _types.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 from typing import TypedDict
+import pandas as pd
 
 
 class FlightPoint(TypedDict):
@@ -9,12 +10,26 @@ class FlightPoint(TypedDict):
     altitude: float
     level: float
     thrust: float
+    time: pd.Timestamp
+    aircraft_mass: float
 
+
+class Objectives(TypedDict):
+    contrail: float
+    co2: float
+    time: float
+    cocip: float
+
+
+FlightPath = list[FlightPoint]
 
 IndexPoint3D = namedtuple("IndexPoint3D", "lat lon alt")
+IndexPath = list[IndexPoint3D]
+
 Point2D = namedtuple("Point2D", "lat lon")
 Point3D = namedtuple("Point3D", "lat lon alt")
 Point4D = namedtuple("Point4D", "lat lon alt time")
+
 Path2D = list[Point2D]
 Grid2D = list[list[Point2D]]
 Grid3D = dict[int, Grid2D]
--- performance_model/flight.py
+++ performance_model/flight.py
@@ -1,44 +1,69 @@
 import pandas as pd
 from utils import Conversions
-from datetime import datetime
+from routing_graph import RoutingGraphManager
+from _types import FlightPath, IndexPath, Objectives, IndexPoint3D
+from config import Config
+from performance_model import PerformanceModel
 
 
 class Flight:
-    def __init__(self, routing_graph_manager, flight_path, config):
-        self.routing_graph_manager = routing_graph_manager
-        self.performance_model = routing_graph_manager.get_performance_model()
-        self.flight_path = flight_path
-        self.config = config
-        self.indices = []
-        self.objectives = None
+    def __init__(
+        self,
+        routing_graph_manager: RoutingGraphManager,
+        flight_path: FlightPath,
+        config: Config,
+    ):
+        """
+        Class to hold a flight path and relevant objective information
+        """
+        self.routing_graph_manager: RoutingGraphManager = routing_graph_manager
+        self.performance_model: PerformanceModel = (
+            routing_graph_manager.get_performance_model()
+        )
+        self.flight_path: FlightPath = flight_path
+        self.config: Config = config
+        self.indices: IndexPath = []
+        self.objectives: Objectives or None = None
 
-    def set_departure(self, departure):
+    def set_departure(self, departure: IndexPoint3D) -> None:
+        """
+        Sets the departure point for the flight path
+        """
         self.add_point_from_index(departure)
         point = self.flight_path[0]
         point["time"] = self.config.DEPARTURE_DATE
         point["aircraft_mass"] = self.config.STARTING_WEIGHT
         self.flight_path[0] = point
 
-    def run_performance_model(self):
+    def run_performance_model(self) -> None:
+        """
+        Runs the performance model on the flight path
+        """
         self.flight_path = self.performance_model.run_apm(self.flight_path)
 
-    def add_point_from_index(self, index):
+    def add_point_from_index(self, index: IndexPoint3D) -> None:
+        """
+        Adds an index point to the index path
+        """
         self.indices.append(index)
         point = self.routing_graph_manager.convert_index_to_point(
             index,
         )
         self.flight_path.append(point)
 
-    def set_objective_value(self, objective_values):
+    def set_objective_value(self, objective_values: Objectives) -> None:
+        """
+        Sets the objective values for this flight path
+        """
         self.objectives = objective_values
 
 
 class RealFlight(Flight):
     def __init__(
         self,
-        flight_name,
-        routing_graph_manager,
-        config,
+        flight_name: str,
+        routing_graph_manager: RoutingGraphManager,
+        config: Config,
     ):
         super().__init__(
             routing_graph_manager,
@@ -46,10 +71,13 @@ class RealFlight(Flight):
             config,
         )
         self.flight_path = pd.read_csv(f"data/{flight_name}")
-        self.flight_path = self.convert_real_flight_path()
-        self.config = config
+        self.flight_path: FlightPath = self.convert_real_flight_path()
+        self.config: Config = config
 
-    def convert_real_flight_path(self):
+    def convert_real_flight_path(self) -> FlightPath:
+        """
+        Converts a dataframe of a real flight path to a list of dictionaries
+        """
         path = []
         for _, row in self.flight_path.iterrows():
             path_point = {
@@ -69,7 +97,10 @@ class RealFlight(Flight):
 
         return path
 
-    def calculate_objectives(self):
+    def calculate_objectives(self) -> Objectives:
+        """
+        Calculates the objective values for this flight path
+        """
         objectives = {}
         for objective in self.config.OBJECTIVES:
             objective = objective(self.performance_model, self.config)
