5481b5a284213d477b0fd96ef66d2a36a641118f
Missing-Tech <josephgrabski31@gmail.com>
2024-03-28 14:35:49 +0000

    Add types for config

--- config.py
+++ config.py
@@ -1,86 +1,97 @@
 import pandas as pd
 from objectives import ContrailObjective, CO2Objective, TimeObjective, CocipObjective
 
+import typing
+
+if typing.TYPE_CHECKING:
+    from _types import Point2D
+    from objectives import Objective
+
 
 class Config:
-    NAME = "overall"
+    NAME: str = "overall"
     # Flight Path
-    DEPARTURE_AIRPORT = (50.74045, -3.00313)
-    DESTINATION_AIRPORT = (41.82151, -71.12919)  # ToD for the real flight path
-    DEPARTURE_DATE = pd.Timestamp(
+    DEPARTURE_AIRPORT: "Point2D" = (50.74045, -3.00313)
+    DESTINATION_AIRPORT: "Point2D" = (
+        41.82151,
+        -71.12919,
+    )  # ToD for the real flight path
+    DEPARTURE_DATE: pd.Timestamp = pd.Timestamp(
         year=2024, month=1, day=31, hour=13, minute=45, second=57
     )
-    WEATHER_BOUND = pd.Timedelta("12h") - pd.Timedelta("45m") - pd.Timedelta("57s")
-    NO_OF_POINTS = 10
-    GRID_WIDTH = 40
-    GRID_SPACING = 20  # km
+    WEATHER_BOUND: pd.Timedelta = (
+        pd.Timedelta("12h") - pd.Timedelta("45m") - pd.Timedelta("57s")
+    )
+    NO_OF_POINTS: int = 10
+    GRID_WIDTH: int = 40
+    GRID_SPACING: int = 20  # km
 
     # ACO
-    EVAPORATION_RATE = 0.3
-    CO2_WEIGHT = 1
-    CONTRAIL_WEIGHT = 1
-    TIME_WEIGHT = 1
-    PHEROMONE_WEIGHT = 2
-    HEURISTIC_WEIGHT = 1
-    TAU_MIN = 0.1
-    TAU_MAX = 1
-    NO_OF_ANTS = 8
-    NO_OF_ITERATIONS = 1
+    EVAPORATION_RATE: float = 0.3
+    CO2_WEIGHT: float = 1
+    CONTRAIL_WEIGHT: float = 1
+    TIME_WEIGHT: float = 1
+    PHEROMONE_WEIGHT: float = 2
+    HEURISTIC_WEIGHT: float = 1
+    TAU_MIN: float = 0.1
+    TAU_MAX: float = 1
+    NO_OF_ANTS: int = 8
+    NO_OF_ITERATIONS: int = 1
 
     # Aircraft
-    AIRCRAFT_TYPE = "B77W"
-    N_ENGINES = 4
-    WINGSPAN = 60.920
-    STARTING_WEIGHT = 240000
-    NOMINAL_ENGINE_EFFICIENCY = 0.33
-    NOMINAL_FUEL_FLOW = 1.8
+    AIRCRAFT_TYPE: str = "B77W"
+    N_ENGINES: int = 4
+    WINGSPAN: float = 60.920
+    STARTING_WEIGHT: float = 240000
+    NOMINAL_ENGINE_EFFICIENCY: float = 0.33
+    NOMINAL_FUEL_FLOW: float = 1.8
 
     # Search Constraints
-    FLIGHT_LEVELS = [310, 330, 350, 370, 390]
-    PRESSURE_LEVELS = (300, 250, 200)
-    STARTING_ALTITUDE = 31000
-    INITIAL_THRUST = 0.83
-    MAX_THRUST_VAR = 0.01
-    MAX_THRUST = 0.83
-    NOMINAL_THRUST = 0.83
-    MAX_ALTITUDE = 39000
-    MAX_ALTITUDE_VAR = 2000
-    ALTITUDE_STEP = 2000
-    OFFSET_VAR = 20
+    FLIGHT_LEVELS: list[int] = [310, 330, 350, 370, 390]
+    PRESSURE_LEVELS: list[int] = [300, 250, 200]
+    STARTING_ALTITUDE: float = 31000
+    INITIAL_THRUST: float = 0.83
+    MAX_THRUST_VAR: float = 0.01
+    MAX_THRUST: float = 0.83
+    NOMINAL_THRUST: float = 0.83
+    MAX_ALTITUDE: int = 39000
+    MAX_ALTITUDE_VAR: int = 4000
+    ALTITUDE_STEP: int = 2000
+    OFFSET_VAR: int = 20
 
     # Objective Functions
-    OBJECTIVES = [ContrailObjective, CO2Objective, TimeObjective]
+    OBJECTIVES: list["Objective"] = [ContrailObjective, CO2Objective, TimeObjective]
 
     # Earth radius in km
-    R = 6371
+    R: int = 6371
 
 
 # Uses gridded CoCiP data
 class ContrailConfig(Config):
-    NAME = "contrail"
-    OBJECTIVES = [ContrailObjective]
-    CONTRAIL_WEIGHT = 1
+    NAME: str = "contrail"
+    OBJECTIVES: list["Objective"] = [ContrailObjective]
+    CONTRAIL_WEIGHT: float = 1
 
 
 class CocipConfig(Config):
-    NAME = "cocip"
-    OBJECTIVES = [CocipObjective, CO2Objective, TimeObjective]
-    CONTRAIL_WEIGHT = 1
+    NAME: str = "cocip"
+    OBJECTIVES: list["Objective"] = [CocipObjective, CO2Objective, TimeObjective]
+    CONTRAIL_WEIGHT: float = 1
 
 
 class CO2Config(Config):
-    NAME = "co2"
-    OBJECTIVES = [CO2Objective]
-    CO2_WEIGHT = 1
+    NAME: str = "co2"
+    OBJECTIVES: list["Objective"] = [CO2Objective]
+    CO2_WEIGHT: float = 1
 
 
 class TimeConfig(Config):
-    NAME = "time"
-    OBJECTIVES = [TimeObjective]
-    TIME_WEIGHT = 1
+    NAME: str = "time"
+    OBJECTIVES: list["Objective"] = [TimeObjective]
+    TIME_WEIGHT: float = 1
 
 
 class ContrailMaxConfig(Config):
-    NAME = "contrail_max"
-    OBJECTIVES = [ContrailObjective]
-    CONTRAIL_WEIGHT = -10
+    NAME: str = "contrail_max"
+    OBJECTIVES: list["Objective"] = [ContrailObjective]
+    CONTRAIL_WEIGHT: float = -10
